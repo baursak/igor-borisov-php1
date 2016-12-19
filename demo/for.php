c03dbab760d1c35cd8f53c789b6b321012f26068
baursak <baursak@gmail.com>
2016-12-19 18:56:29 +0300

    day 2 - 01h38m49s - table.php - multiplication table

---
+++ demo/for.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by M. Grishkov on 19.12.2016
+ */
+$str_result = "";
+for ($i = 1; $i <= 50; $i+=2){
+	$str_result .="$i ";
+}
+echo $str_result;
---
+++ demo/while.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by M. Grishkov on 19.12.2016
+ */
+$var = "HELLO";
+$i = 0;
+while($i < strlen($var)){
+	echo $var{$i} . " ";
+	$i++;
+}
